38aed605663de7c213197cb0ad85f0115a17f020
melzreal <melissa.real@hotmail.com>
2020-04-02 09:50:16 +0100

    finished mini project

--- bookingsystem/script.js
+++ bookingsystem/script.js
@@ -3,22 +3,56 @@ const container = document.querySelector('.container');
 //queryselector all treats the elements selected as an array
 //so that way we can run array methods on them
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
-
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
 //adding a plus sign turns your value from a string into a number
 let ticketPrice = +movieSelect.value;
 
+//save movie index and price
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+
+}
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
+
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 
 })
 
+
+
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    //localstorage is built into the browser so we dont need to import it
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
@@ -33,4 +67,6 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
     }
 })
+
+updateSelectedCount();
 
